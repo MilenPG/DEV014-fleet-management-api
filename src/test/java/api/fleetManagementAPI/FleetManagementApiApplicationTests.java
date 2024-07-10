a508f49eac9a9d66648f1ef3b797ced4da9e20f6
Milen <millalen.pg@gmail.com>
2024-07-10 04:21:08 -0400

    first trial controller that returns hello world for a GET request

---
+++ src/test/java/api/fleetManagementAPI/FleetManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package api.fleetManagementAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
