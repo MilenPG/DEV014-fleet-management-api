88ba2c8387d53f12944e819988f2219ae2757bc2
Milen <millalen.pg@gmail.com>
2024-07-28 13:05:14 -0400

    clean(trajectory): delete some comments at repository and controller

--- src/main/java/api/fleetManagementAPI/repositories/TrajectoryRepository.java
+++ src/main/java/api/fleetManagementAPI/repositories/TrajectoryRepository.java
@@ -22,13 +22,6 @@ public interface TrajectoryRepository extends JpaRepository<Trajectory, Integer>
                     "FROM public.TRAJECTORIES) " +
 
                     "AS ranked " +
-                    "WHERE row_num = 1") // producto de la partición ordenada descendentemente, la columna 1 sería el grupo requerido
+                    "WHERE row_num = 1")   // producto de la partición ordenada descendentemente, la columna 1 sería el grupo requerido
     List<Trajectory> findByLatestTrajectories(Pageable page);
-
-    //las fechas están guardadas como timeStamp, que incluye hasta la hora, pero el endpoint solo nos pide fecha (convertir timeStamp a String para comparar)
-    /*- El método a crear en TrajectoryRepository, debe convertir el tipo de dato timeStamp a String
-	- ?? Esto para que pueda aplicarse Contains y así retorne todos aquellos elementos que contengan
-	los caracteres pasados por parámetro ??
-	(ej: para 2 trayectorias con distinto horario pero misma fecha 12-10-2024830, 12-10-2024T14:45,
-	si busco 12-10-2024, sabrá retornar ambas trayectorias, pues basta que ambas contengan la porción de String dada.) */
 }
